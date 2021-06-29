920a812089b05bee2024ab017b36a7eafa30b5b7
Daniel Santana <daniel@everymundo.com>
2021-06-29 15:05:19 -0400

    prepare serverless.yml to serverless version 3

--- layer-description.js
+++ layer-description.js
@@ -1,1 +1,15 @@
-module.exports = () => Object.entries(require("./nodejs/package.json").dependencies).map(([k,v])=>`${k}@${v}`).join("\n")
+const path = require('path')
+
+module.exports = function () {
+  try {
+    return {
+      description: Object
+        .entries(require(path.join(__dirname, 'nodejs/package.json')).dependencies)
+        .map(([k, v]) => `${k}@${v}`).join('\n')
+    }
+  } catch (e) {
+    console.error('DESCRIPTION ERROR:\n', e.message, '\n')
+
+    return { description: '' }
+  }
+}
